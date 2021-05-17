f9f5373bb6253b2bf8fce9175de50471ca1b1f4b
Vinicius Lunardi Farias <vinilunardi00@gmail.com>
2021-05-17 22:05:57 +0000

    adicionando prettier

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -4,6 +4,6 @@ import * as module_alias from 'module-alias';
 const files = path.resolve(__dirname, '../..');
 
 module_alias.addAliases({
-	'@src': path.join(files, 'src'),
-	'@test': path.join(files, 'test'),
+  '@src': path.join(files, 'src'),
+  '@test': path.join(files, 'test'),
 });
