9405c5d8f0833038ddc384b4d396f6c69c9986f2
Vinicius Lunardi Farias <vinilunardi00@gmail.com>
2021-05-19 23:08:47 +0000

    adicionando mongoose e database

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,19 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from '@src/controllers/forecast';
+import { BeachesController } from '@src/controllers/beaches';
 import { Application } from 'express';
+import * as database from '@src/./database'
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -20,9 +23,18 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
-
+  
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+  
+  public async close(): Promise<void> {
+    await database.close();
+  }
+  
   public getApp(): Application {
     return this.app;
   }
