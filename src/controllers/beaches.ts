189e55e035ec7919ba40f3900132785c01cf976f
Vinicius Lunardi Farias <vinilunardi00@gmail.com>
2021-05-24 22:56:02 +0000

    finalizando testes

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -5,18 +5,18 @@ import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
-	@Post('')
-	public async create(req: Request, res: Response): Promise<Response> {
-		try {
-			const beach = new Beach(req.body);
-			const result = await beach.save();
-			
-			return res.status(201).send(result);
-		} catch (err) {
-			if (err instanceof mongoose.Error.ValidationError) {
-				return res.status(422).send({ error: err.message });
-			}
-			return res.status(500).send({ error: 'Internal Server Error' });
-		}
-	}
+  @Post('')
+  public async create(req: Request, res: Response): Promise<Response> {
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
+
+      return res.status(201).send(result);
+    } catch (err) {
+      if (err instanceof mongoose.Error.ValidationError) {
+        return res.status(422).send({ error: err.message });
+      }
+      return res.status(500).send({ error: 'Internal Server Error' });
+    }
+  }
 }
--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,37 +1,37 @@
 import { Beach, BeachPosition } from '@src/models/beach';
 
 describe('Beaches functional tests', () => {
-	beforeAll(async () => await Beach.deleteMany({}));
-	
-	describe('When creating a beach', () => {
-		it('should create a beach with success', async () => {
-			const newBeach = {
-				lat: -33.792726,
-				lng: 151.289824,
-				name: 'Manly',
-				position: BeachPosition.E,
-			};
-			
-			const response = await global.testRequest.post('/beaches').send(newBeach);
-			expect(response.status).toBe(201);
-			expect(response.body).toEqual(expect.objectContaining(newBeach));
-		});
-		
-		it('should return 422 when there is a validation error', async () => {
-			const newBeach = {
-				lat: 'invalid_string',
-				lng: 151.289824,
-				name: 'Manly',
-				position: BeachPosition.E,
-			};
-			
-			const response = await global.testRequest.post('/beaches').send(newBeach);
-			expect(response.status).toBe(422);
-			expect(response.body).toEqual({
-				error:
-					'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
-			});
-		});
-	});
-	afterAll(async () => await Beach.deleteMany({}));
+  beforeAll(async () => await Beach.deleteMany({}));
+
+  describe('When creating a beach', () => {
+    it('should create a beach with success', async () => {
+      const newBeach = {
+        lat: -33.792726,
+        lng: 151.289824,
+        name: 'Manly',
+        position: BeachPosition.E,
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: BeachPosition.E,
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
+    });
+  });
+  afterAll(async () => await Beach.deleteMany({}));
 });
