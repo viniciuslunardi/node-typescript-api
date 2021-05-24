0adee7cf5493e3e10c5ef965c52b223dbb2bb72f
Vinicius Lunardi Farias <vinilunardi00@gmail.com>
2021-05-24 23:23:33 +0000

    aaa

--- src/server.ts
+++ src/server.ts
@@ -7,35 +7,41 @@ import { Application } from 'express';
 import * as database from '@src/./database';
 
 export class SetupServer extends Server {
-  constructor(private port = 3000) {
-    super();
-  }
-
-  public async init(): Promise<void> {
-    this.setupExpress();
-    this.setupControllers();
-    await this.databaseSetup();
-  }
-
-  private setupExpress(): void {
-    this.app.use(bodyParser.json());
-  }
-
-  private setupControllers(): void {
-    const forecastController = new ForecastController();
-    const beachesController = new BeachesController();
-    this.addControllers([forecastController, beachesController]);
-  }
-
-  private async databaseSetup(): Promise<void> {
-    await database.connect();
-  }
-
-  public async close(): Promise<void> {
-    await database.close();
-  }
-
-  public getApp(): Application {
-    return this.app;
-  }
+	constructor(private port = 3000) {
+		super();
+	}
+	
+	public async init(): Promise<void> {
+		this.setupExpress();
+		this.setupControllers();
+		await this.databaseSetup();
+	}
+	
+	private setupExpress(): void {
+		this.app.use(bodyParser.json());
+	}
+	
+	private setupControllers(): void {
+		const forecastController = new ForecastController();
+		const beachesController = new BeachesController();
+		this.addControllers([forecastController, beachesController]);
+	}
+	
+	private async databaseSetup(): Promise<void> {
+		await database.connect();
+	}
+	
+	public async close(): Promise<void> {
+		await database.close();
+	}
+	
+	public start(): void {
+		this.app.listen(this.port, () => {
+			console.info(`Server listening on port ${ this.port }`);
+		});
+	}
+	
+	public getApp(): Application {
+		return this.app;
+	}
 }
